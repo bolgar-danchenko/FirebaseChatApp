7a660c13a7f241522d7ecc89203db6be87b73716
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2023-01-24 20:06:16 +0400

    Initial Commit

---
+++ FirebaseChatApp/FirebaseChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatAppApp.swift
+//  FirebaseChatApp
+//
+//  Created by Konstantin Bolgar-Danchenko on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
