1d2852a9916c6007071c45d45efb935df81f8389
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2023-01-25 21:35:29 +0400

    Fetching current user from Firestore

---
+++ FirebaseChatApp/Utils/FirebaseManager.swift
@@ -0,0 +1,28 @@
+//
+//  FirebaseManager.swift
+//  FirebaseChatApp
+//
+//  Created by Konstantin Bolgar-Danchenko on 25.01.2023.
+//
+
+import Foundation
+import Firebase
+import FirebaseStorage
+import FirebaseFirestore
+
+class FirebaseManager: NSObject {
+    
+    let auth: Auth
+    let storage: Storage
+    let firestore: Firestore
+    
+    static let shared = FirebaseManager()
+    
+    override init() {
+        FirebaseApp.configure()
+        self.auth = Auth.auth()
+        self.storage = Storage.storage()
+        self.firestore = Firestore.firestore()
+        super.init()
+    }
+}
